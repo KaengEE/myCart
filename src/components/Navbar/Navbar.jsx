a93269bfb171576b58d9a0accd980bc77ecae187
KaengEE <yopuiqwer@naver.com>
2023-12-19 09:44:14 +0900

    장바구니

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -32,6 +32,10 @@ const Navbar = () => {
         <LinkWithIcon title="가입" link="/signup" emoji={memo} />
         <LinkWithIcon title="내주문" link="/myorders" emoji={order} />
         <LinkWithIcon title="로그아웃" link="/logout" emoji={lock} />
+        {/* 장바구니 */}
+        <a href="/cart" className="align_center">
+          장바구니 <p className="align_center cart_counts">0</p>
+        </a>
       </div>
     </nav>
   );
