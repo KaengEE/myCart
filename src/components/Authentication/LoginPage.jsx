8a7f51fd02df05adb5cf43791cf647a501aee7ad
KaengEE <yopuiqwer@naver.com>
2023-12-19 15:01:04 +0900

    유효성검사

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -1,63 +1,51 @@
-import { useRef, useState } from "react";
 import "./LoginPage.css";
+import { useForm } from "react-hook-form";
 
 const LoginPage = () => {
-  //리액트에서 특정 태그를 선택하는 방법
-  const passwordRef = useRef(null);
-
-  //유저 state
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-  });
-
-  //submit 메서드
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(user);
-    // 초기화
-    setUser({ email: "", password: "" });
-  };
+  //react-hook-form
+  const {
+    register,
+    handleSubmit,
+    //유효성 검증 error
+    formState: { errors },
+  } = useForm();
+  const submitData = (formData) => console.log(formData);
 
   return (
     <section className="align_center form_page">
-      <form className="authentication_form" onSubmit={handleSubmit}>
+      <form className="authentication_form" onSubmit={handleSubmit(submitData)}>
         <h2>로그인 폼</h2>
         <div className="form_inputs">
           <div>
             <label htmlFor="email">Email</label>
             <input
+              {...register("email", { required: "이메일을 입력해주세요." })}
               type="email"
               id="email"
               className="form_text_input"
               placeholder="이메일 입력..."
-              onChange={(e) => setUser({ ...user, email: e.target.value })}
-              value={user.email}
             />
+            {/* 에러메시지 */}
+            {errors.email && (
+              <em className="form_error">{errors.email.message}</em>
+            )}
           </div>
           <div>
             <label htmlFor="password">Password</label>
             <input
-              type="password"
-              ref={passwordRef}
+              {...register("password", {
+                required: "패스워드를 입력해주세요.",
+                minLength: { value: 4, message: "패스워드는 최소 4자 이상." },
+              })}
               id="password"
+              type="password"
               className="form_text_input"
               placeholder="패스워드"
-              onChange={(e) => setUser({ ...user, password: e.target.value })}
-              value={user.password}
             />
-            <button
-              type="button"
-              onClick={() => (passwordRef.current.type = "password")}
-            >
-              비밀번호 숨기기
-            </button>
-            <button
-              type="button"
-              onClick={() => (passwordRef.current.type = "text")}
-            >
-              비밀번호 보이게
-            </button>
+            {/* 에러메시지 */}
+            {errors.password && (
+              <em className="form_error">{errors.password.message}</em>
+            )}
           </div>
 
           <button type="submit" className="search_button form_submit">
