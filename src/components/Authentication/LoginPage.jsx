beccc29924775b161b00101d517a88a14c016a30
KaengEE <yopuiqwer@naver.com>
2023-12-19 14:19:02 +0900

    로그인 폼 입력/submit

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -1,12 +1,27 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./LoginPage.css";
 
 const LoginPage = () => {
   //리액트에서 특정 태그를 선택하는 방법
   const passwordRef = useRef(null);
+
+  //유저 state
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  //submit 메서드
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+    // 초기화
+    setUser({ email: "", password: "" });
+  };
+
   return (
     <section className="align_center form_page">
-      <form className="authentication_form">
+      <form className="authentication_form" onSubmit={handleSubmit}>
         <h2>로그인 폼</h2>
         <div className="form_inputs">
           <div>
@@ -16,6 +31,8 @@ const LoginPage = () => {
               id="email"
               className="form_text_input"
               placeholder="이메일 입력..."
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              value={user.email}
             />
           </div>
           <div>
@@ -26,6 +43,8 @@ const LoginPage = () => {
               id="password"
               className="form_text_input"
               placeholder="패스워드"
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              value={user.password}
             />
             <button
               type="button"
