98e3ac7ba8f9c01ba1bd88ad597e05c22e00a398
KaengEE <yopuiqwer@naver.com>
2023-12-20 09:42:44 +0900

    라우팅 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
 import "./App.css";
-import LoginPage from "./components/Authentication/LoginPage";
-import SignupPage from "./components/Authentication/SignupPage";
-import CartPage from "./components/Cart/CartPage";
-import { HomePage } from "./components/Home/Homepage";
-import MyOrderPage from "./components/MyOrder/MyOrderPage";
 import Navbar from "./components/Navbar/Navbar";
-import ProductPage from "./components/Products/ProductPage";
-import SingleProductPage from "./components/SingleProduct/SingleProductPage";
+import Routing from "./components/Routing/Routing";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       <main>
-        {/* <HomePage /> */}
-        {/* <ProductPage /> */}
-        {/* <SingleProductPage /> */}
-        {/* <CartPage /> */}
-        {/* <MyOrderPage /> */}
-        {/* <LoginPage /> */}
-        <SignupPage />
+        <Routing />
       </main>
     </div>
   );
---
+++ src/components/Routing/Routing.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "../Home/Homepage";
+import ProductPage from "../Products/ProductPage";
+import SingleProductPage from "../SingleProduct/SingleProductPage";
+import SignupPage from "../Authentication/SignupPage";
+import LoginPage from "../Authentication/LoginPage";
+import CartPage from "../Cart/CartPage";
+import MyOrderPage from "../MyOrder/MyOrderPage";
+
+const Routing = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/products" element={<ProductPage />} />
+      <Route path="/product/:id" element={<SingleProductPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/myorders" element={<MyOrderPage />} />
+    </Routes>
+  );
+};
+
+export default Routing;
