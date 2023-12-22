ed23a2ad7eed874dd22103d798e638397930567e
KaengEE <yopuiqwer@naver.com>
2023-12-22 11:16:00 +0900

    홈에 대표상품 출력

--- src/components/Home/FeaturedProducts.jsx
+++ src/components/Home/FeaturedProducts.jsx
@@ -1,15 +1,21 @@
+import useData from "../../Hook/useData";
 import ProductCard from "../Products/ProductCard";
+import ProductCardSkeleton from "../Products/ProductCardSkeleton";
 import "./FeaturedProducts.css";
 
 const FeaturedProducts = () => {
+  const { data, error, isLoading } = useData("/products/featured");
+  //console.log(data);
+  const skeletons = [1, 2, 3];
   return (
     <section className="featured_products">
       <h2>주요제품</h2>
 
       <div className="align_center featured_products_list">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {error && <em className="form_error">{error}</em>}
+        {isLoading && skeletons.map((n) => <ProductCardSkeleton key={n} />)}
+        {data &&
+          data.map((data) => <ProductCard key={data._id} product={data} />)}
       </div>
     </section>
   );
